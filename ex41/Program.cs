521bfd18c124030cf07a63392490e252a346add0
maksimussokol <maksimus@2074.ru>
2022-10-07 21:23:06 +0500

    All works

---
+++ ex41/Program.cs
@@ -0,0 +1,19 @@
+﻿void positiv(params int[] m)
+{
+    int count = 0;
+    for (int i = 0; i < m.Length; i++)
+    {
+        if (m[i] > 0) count++;
+    }
+    Console.WriteLine("positive count is {0}", count);
+}
+Console.WriteLine("enter numbers");
+int[] array = Console.ReadLine().
+Split(",").
+Select(item => Convert.
+ToInt32(item)).
+ToArray();
+
+//dinamicheskiy vvod chisel s preobrazovaniem v massiv eto ochen kruto)))
+
+positiv(array);
---
+++ ex43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+void mash(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"tochka peresecheniya ({x}, {y})");
+}
+Console.WriteLine("enter b1");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter k1");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter b2");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter k2");
+double d = Convert.ToDouble(Console.ReadLine());
+
+mash(a, b, c, d);
